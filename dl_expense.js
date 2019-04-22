493d3c37e4afafca36328e5879d332826a326724
Diego Sandoval <dsandoval004@west-mec.org>
2019-04-22 13:27:38 -0700

    Comments
    Added comments

--- dl_expense.js
+++ dl_expense.js
@@ -30,34 +30,34 @@
       Formats the value, "val", as U.S. currency.
       
 */
-// DDOES:
+// DDOES: Adds and listener to run an anonymous function when the page is loaded
 window.addEventListener('load', function () {
-      // DVARO:
+      // DVARO: A collection of all the input with the sum class in the travelExp table
       var changingCells = document.querySelectorAll('table#travelExp input.sum');
-      // DLOOP:
+      // DLOOP: Loops through changinCells and adds a change event listener
       for (var i = 0; i < changingCells.length; i++) {
             changingCells[i].addEventListener('change', calcExp);
       }
-      // DDOES:
+      // DDOES: Adds a function to the the submit button to be run when clicked on
       document.getElementById('submitButton').addEventListener('click', validateSummary);
 });
-// DFUNC:
+// DFUNC: The validateSummary function
 function validateSummary() {
-      // DVARO:
+      // DVARO: gets the element with the id of summary
       var summary = document.getElementById('summary');
-      // DIFDO:
+      // DIFDO: If no value is entered then it say a custom message if their is a value then nothing is displayed
       if (summary.validity.valueMissing) {
             summary.setCustomValidity("You must include a summary of the trip in your report.");
       } else {
             summary.setCustomValidity("");
       }
 };
-// DFUNC:
+// DFUNC: The calcClass function
 function calcClass(sumClass) {
-      // DVARL:
+      // DVARL: Local variables for this function
       var sumFields = document.getElementsByClassName(sumClass),
             sumTotal = 0;
-      // DLOOP:
+      // DLOOP: Loops through sumFields and calculates the total
       for (var i = 0; i < sumFields.length; i++) {
             var itemValue = parseFloat(sumFields[i].value)
             if (isNaN(itemValue) == false) {
@@ -66,14 +66,15 @@ function calcClass(sumClass) {
       }
       return sumTotal;
 };
-// DFUNC:
+// DFUNC: The calcExp function
 function calcExp() {
+      // DVARO: Gets all the tr in the tbody of the travelExp table
       var expTable = document.querySelectorAll('table#travelExp tbody tr');
-      // DLOOP:
+      // DLOOP: Loops throught the expTable and cchanges the value the subtotals
       for (var i = 0; i < expTable.length; i++) {
             document.getElementById(`subtotal${i}`).value = formatNumber(calcClass(`date${i}`), 2);
       }
-      // DDOES:
+      // DDOES: Changes the value of the cells to have a running total
       document.getElementById('transTotal').value = formatNumber(calcClass('trans'), 2);
       document.getElementById('lodgeTotal').value = formatNumber(calcClass('lodge'), 2);
       document.getElementById('mealTotal').value = formatNumber(calcClass('meal'), 2);
